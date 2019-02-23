84fe5472a467d46635a2c337a4f39fcb218ee188
Parag <paragpatel91@gmail.com>
2019-02-23 17:35:48 -0600

    Initial Commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const form = props => {
+  return (
+    <form onSubmit={props.clicked}>
+      <input type="text" name="city" placeholder="City.." />
+      <input type="text" name="country" placeholder="Country.." />
+      <button>Get Weather</button>
+    </form>
+  );
+};
+export default form;
